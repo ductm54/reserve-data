8e5a1ccb1cbc95e2e29c2f85f34ca3a72bed139e
Hoàng Hà <halink0803@gmail.com>
2018-07-27 11:33:19 +0700

    update receiver names

--- common/blockchain/nonce/auto_increasing.go
+++ common/blockchain/nonce/auto_increasing.go
@@ -10,12 +10,14 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+//AutoIncreasing is used for manually increase transaction nonce
 type AutoIncreasing struct {
 	address     ethereum.Address
 	mu          sync.Mutex
 	manualNonce *big.Int
 }
 
+//NewAutoIncreasing return a new AutoIncreasing instance
 func NewAutoIncreasing(
 	address ethereum.Address) *AutoIncreasing {
 	return &AutoIncreasing{
@@ -25,37 +27,40 @@ func NewAutoIncreasing(
 	}
 }
 
-func (self *AutoIncreasing) GetAddress() ethereum.Address {
-	return self.GetAddress()
+//GetAddress return a AutoIncreasing addresss
+func (ai *AutoIncreasing) GetAddress() ethereum.Address {
+	return ai.GetAddress()
 }
 
-func (self *AutoIncreasing) getNonceFromNode(ethclient *ethclient.Client) (*big.Int, error) {
+func (ai *AutoIncreasing) getNonceFromNode(ethclient *ethclient.Client) (*big.Int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
-	nonce, err := ethclient.PendingNonceAt(ctx, self.GetAddress())
+	nonce, err := ethclient.PendingNonceAt(ctx, ai.GetAddress())
 	return big.NewInt(int64(nonce)), err
 }
 
-func (self *AutoIncreasing) MinedNonce(ethclient *ethclient.Client) (*big.Int, error) {
+//MinedNonce return nonce which is mined
+func (ai *AutoIncreasing) MinedNonce(ethclient *ethclient.Client) (*big.Int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
-	nonce, err := ethclient.NonceAt(ctx, self.GetAddress(), nil)
+	nonce, err := ethclient.NonceAt(ctx, ai.GetAddress(), nil)
 	return big.NewInt(int64(nonce)), err
 }
 
-func (self *AutoIncreasing) GetNextNonce(ethclient *ethclient.Client) (*big.Int, error) {
-	self.mu.Lock()
-	defer self.mu.Unlock()
-	nodeNonce, err := self.getNonceFromNode(ethclient)
+//GetNextNonce return next transaction nonce
+func (ai *AutoIncreasing) GetNextNonce(ethclient *ethclient.Client) (*big.Int, error) {
+	ai.mu.Lock()
+	defer ai.mu.Unlock()
+	nodeNonce, err := ai.getNonceFromNode(ethclient)
 	if err != nil {
 		return nodeNonce, err
 	} else {
-		if nodeNonce.Cmp(self.manualNonce) == 1 {
-			self.manualNonce = big.NewInt(0).Add(nodeNonce, ethereum.Big1)
+		if nodeNonce.Cmp(ai.manualNonce) == 1 {
+			ai.manualNonce = big.NewInt(0).Add(nodeNonce, ethereum.Big1)
 			return nodeNonce, nil
 		} else {
-			nextNonce := self.manualNonce
-			self.manualNonce = big.NewInt(0).Add(nextNonce, ethereum.Big1)
+			nextNonce := ai.manualNonce
+			ai.manualNonce = big.NewInt(0).Add(nextNonce, ethereum.Big1)
 			return nextNonce, nil
 		}
 	}
