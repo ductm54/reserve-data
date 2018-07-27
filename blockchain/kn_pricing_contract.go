6d77f85f7abd1d6d99c69e5b8834978e6fb692ba
Hoàng Hà <halink0803@gmail.com>
2018-07-27 13:40:06 +0700

    update receiver names

--- blockchain/kn_pricing_contract.go
+++ blockchain/kn_pricing_contract.go
@@ -10,34 +10,34 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
-func (self *Blockchain) GeneratedSetBaseRate(opts blockchain.TxOpts, tokens []ethereum.Address, baseBuy []*big.Int, baseSell []*big.Int, buy [][14]byte, sell [][14]byte, blockNumber *big.Int, indices []*big.Int) (*types.Transaction, error) {
+func (b *Blockchain) GeneratedSetBaseRate(opts blockchain.TxOpts, tokens []ethereum.Address, baseBuy []*big.Int, baseSell []*big.Int, buy [][14]byte, sell [][14]byte, blockNumber *big.Int, indices []*big.Int) (*types.Transaction, error) {
 	timeout, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	return self.BuildTx(timeout, opts, self.pricing, "setBaseRate", tokens, baseBuy, baseSell, buy, sell, blockNumber, indices)
+	return b.BuildTx(timeout, opts, b.pricing, "setBaseRate", tokens, baseBuy, baseSell, buy, sell, blockNumber, indices)
 }
 
-func (self *Blockchain) GeneratedSetCompactData(opts blockchain.TxOpts, buy [][14]byte, sell [][14]byte, blockNumber *big.Int, indices []*big.Int) (*types.Transaction, error) {
+func (b *Blockchain) GeneratedSetCompactData(opts blockchain.TxOpts, buy [][14]byte, sell [][14]byte, blockNumber *big.Int, indices []*big.Int) (*types.Transaction, error) {
 	timeout, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	return self.BuildTx(timeout, opts, self.pricing, "setCompactData", buy, sell, blockNumber, indices)
+	return b.BuildTx(timeout, opts, b.pricing, "setCompactData", buy, sell, blockNumber, indices)
 }
 
-func (self *Blockchain) GeneratedSetImbalanceStepFunction(opts blockchain.TxOpts, token ethereum.Address, xBuy []*big.Int, yBuy []*big.Int, xSell []*big.Int, ySell []*big.Int) (*types.Transaction, error) {
+func (b *Blockchain) GeneratedSetImbalanceStepFunction(opts blockchain.TxOpts, token ethereum.Address, xBuy []*big.Int, yBuy []*big.Int, xSell []*big.Int, ySell []*big.Int) (*types.Transaction, error) {
 	timeout, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	return self.BuildTx(timeout, opts, self.pricing, "setImbalanceStepFunction", token, xBuy, yBuy, xSell, ySell)
+	return b.BuildTx(timeout, opts, b.pricing, "setImbalanceStepFunction", token, xBuy, yBuy, xSell, ySell)
 }
 
-func (self *Blockchain) GeneratedSetQtyStepFunction(opts blockchain.TxOpts, token ethereum.Address, xBuy []*big.Int, yBuy []*big.Int, xSell []*big.Int, ySell []*big.Int) (*types.Transaction, error) {
+func (b *Blockchain) GeneratedSetQtyStepFunction(opts blockchain.TxOpts, token ethereum.Address, xBuy []*big.Int, yBuy []*big.Int, xSell []*big.Int, ySell []*big.Int) (*types.Transaction, error) {
 	timeout, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	return self.BuildTx(timeout, opts, self.pricing, "setQtyStepFunction", token, xBuy, yBuy, xSell, ySell)
+	return b.BuildTx(timeout, opts, b.pricing, "setQtyStepFunction", token, xBuy, yBuy, xSell, ySell)
 }
 
-func (self *Blockchain) GeneratedGetRate(opts blockchain.CallOpts, token ethereum.Address, currentBlockNumber *big.Int, buy bool, qty *big.Int) (*big.Int, error) {
+func (b *Blockchain) GeneratedGetRate(opts blockchain.CallOpts, token ethereum.Address, currentBlockNumber *big.Int, buy bool, qty *big.Int) (*big.Int, error) {
 	timeOut := 2 * time.Second
 	out := big.NewInt(0)
-	err := self.Call(timeOut, opts, self.pricing, out, "getRate", token, currentBlockNumber, buy, qty)
+	err := b.Call(timeOut, opts, b.pricing, out, "getRate", token, currentBlockNumber, buy, qty)
 	return out, err
 }
 
