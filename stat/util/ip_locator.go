88be7fea0d23bdeea0cc5510d21db8f944eb5fd1
Diep Pham <me@favadi.com>
2018-09-21 11:55:49 +0700

    stop IPToCountry to return an error when country information is not available from database

--- stat/util/ip_locator.go
+++ stat/util/ip_locator.go
@@ -11,7 +11,12 @@ import (
 	"github.com/KyberNetwork/reserve-data/common"
 )
 
-const geoDBFile = "GeoLite2-Country.mmdb"
+const (
+	geoDBFile = "GeoLite2-Country.mmdb"
+
+	// UnknownCountry is returned when IP locator can't find the country of IP in database
+	UnknownCountry = "unknown"
+)
 
 // IPLocator is a resolver that query data of IP from MaxMind's GeoLite2 database.
 type IPLocator struct {
@@ -29,6 +34,7 @@ func NewIPLocator() (*IPLocator, error) {
 }
 
 // IPToCountry returns the country of given IP address.
+// When country of given IP does not exists in database, it returns unknown.
 func (il *IPLocator) IPToCountry(ip string) (string, error) {
 	IPParsed := net.ParseIP(ip)
 	if IPParsed == nil {
@@ -42,7 +48,8 @@ func (il *IPLocator) IPToCountry(ip string) (string, error) {
 
 	country := record.Country.IsoCode //iso code of country
 	if country == "" {
-		return "", fmt.Errorf("Can't find country of the given ip: %s", ip)
+		log.Printf("Can't find country of the given ip: %s", ip)
+		country = UnknownCountry
 	}
 	return country, nil
 }
